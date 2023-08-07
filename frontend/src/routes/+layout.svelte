00d68a644c460f304824524b817a0b209a1805ca
Yassin Siouda <yassin@siouda.com>
2023-08-07 22:16:27 +0200

    chore(front): wip mopidy wss attempts

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,9 +1,14 @@
-<script>
-    import "../app.css";
-    import { onMount } from "svelte";
-	import { watchUserChange } from "../lib/store";
-	import Nav from "$lib/components/Nav.svelte";
-	import Player from "$lib/components/Player.svelte";
+<script lang="ts">
+  import "../app.css";
+  import { onMount } from "svelte";
+  import { watchUserChange } from "../lib/store";
+  import Nav from "$lib/components/Nav.svelte";
+  import Player from "$lib/components/Player.svelte";
+  import Mopidy from "mopidy";
+
+  const mopidy = new Mopidy({
+  webSocketUrl: "ws://localhost:6680/mopidy/ws/",
+  });
 
     onMount(() => {
        watchUserChange();
