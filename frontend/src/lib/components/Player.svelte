a1481daa97f02aa056a0502241c8d88b0b414ee5
Yassin <yassin@siouda.com>
2023-09-04 15:35:36 +0200

    chore(front): wip snapcast stream attempts

--- frontend/src/lib/components/Player.svelte
+++ frontend/src/lib/components/Player.svelte
@@ -3,25 +3,16 @@
     import type { Player } from "$lib/models/player";
 	import { onMount } from "svelte";
     import SnapStream from '$lib/utils/snapstream';
+    import SnapControl from '$lib/utils/snapcontrol';
     import Audiostream from '$lib/utils/snapstream';
 
     let snapStream;
 
-    function handleContextReady() {
-        snapStream.play();
-    }
+    onMount(() => {
+        //let snapcontrol = new SnapControl("ws://localhost:1780");
+        let setStream = new SnapStream("ws://localhost:1780");
+    });
 
-    function connectSnapStream() {
-        snapStream = new Audiostream();
-        snapStream.on('contextReady', handleContextReady);  
-
-    }
-
-    function stopSnapStream() {
-        snapStream.stop();
-    }
-
-   
     
     function returnPlayerState (state) {
         // if state stopped, play if paused play if playing pause
@@ -38,14 +29,13 @@
 
 </script>
 <section class="fixed bottom-0 bg-primary w-full text-white">
-
-    <button on:click={connectSnapStream}>Connect SnapStream</button>
-    <button on:click={stopSnapStream}>Stop SnapStream</button>
-
+    <div class="bg-red-600">
+        <p>SnapStream</p>
+    
+    </div>
 
     PLAYER
     {#if $player}
         <button on:click={() => $player.state = returnPlayerState($player.state)}>{ $player.state }</button>
     {/if}
-
 </section>
